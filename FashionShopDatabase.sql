e3347ca1f6fbb163342e92fc92ae1aef1fe2f213
Khoa Virgo <ndangkhoa567@gmail.com>
2023-02-11 17:27:50 +0700

    Update

--- FashionShopDatabase.sql
+++ FashionShopDatabase.sql
@@ -1,4 +1,4 @@
-use master
+﻿use master
 
 if exists(select name from sysdatabases where name = 'FashionShop')
 	drop database FashionShop
@@ -9,32 +9,79 @@ if exists(select name from sysobjects where name = 'Users')
 	drop table Users
 create table Users
 (
-	id BIGINT primary key,
-	firstName char(20),
-	lastName char(20),
-	phoneNumber char(10),
-	email char(150),
-	password varchar,
-	admin tinyint,
-	adress varchar(150),
-	province char(30),
-	city char(30),
-	country char(30)
+	user_id BIGINT primary key,
+	firstName nchar(20) not null,
+	lastName nchar(20) not null,
+	phoneNumber nchar(10) not null,
+	email nchar(150) not null,
+	password varchar not null,
+	admin tinyint not null,
+	adress nvarchar(150) not null,
+	province nchar(30) not null,
+	city nchar(30) not null,
+	country nchar(30) not null
 )
 
-if exists(select name from sysobjects where name = 'Users')
-	drop table Users
-create table Users
+if exists(select name from sysobjects where name = 'Products')
+	drop table Products
+create table Products
 (
-	id BIGINT primary key,
-	firstName char(20),
-	lastName char(20),
-	phoneNumber char(10),
-	email char(150),
-	password varchar,
-	admin tinyint,
-	adress varchar(150),
-	province char(30),
-	city char(30),
-	country char(30)
+	product_id bigint primary key,
+	name nvarchar(50) not null,
+	descript text,
+	imageURL nvarchar(100),
+	price float not null,
+	category_id int,
+	brand_id int,
+	product_availability nvarchar(40) not null check (product_availability IN('Hết hàng','Có sẵn','Đặt trước'))
+)
+
+if exists(select name from sysobjects where name = 'Orders')
+	drop table Orders
+create table Orders
+(
+	order_id bigint primary key,
+	customer_id bigint not null,
+	order_date datetime not null,
+	shipping_address nvarchar(100) not null,
+	original_price float not null,
+	reduced_price float,
+	total_price float not null,
+	payment_id bigint not null,
+	voucher_id bigint,
+	order_status nvarchar(40) not null check(order_status IN('Đang xử lý','Đang giao','Đã giao'))
+)
+
+if exists(select name from sysobjects where name = 'Cart')
+	drop table Cart
+create table Cart
+(
+	cart_id bigint primary key,
+	user_id bigint not null,
+	product_id bigint not null,
+	quantity smallint not null,
+	size varchar(5) not null,
+	color varchar(10) not null,
+	total_price float not null
+)
+
+if exists(select name from sysobjects where name = 'Category')
+	drop table Category
+create table Category
+(
+	category_id bigint primary key,
+	parent_id bigint,
+	name nvarchar(20) not null
+)
+
+if exists(select name from sysobjects where name = 'Voucher')
+	drop table Voucher
+create table Voucher
+(
+	voucher_id bigint primary key,
+	code varchar(10) not null,
+	discount_percentage int not null,
+	voucher_status nvarchar(40) not null check(voucher_status IN('Chưa sử dụng','Đã sử dụng')),
+	startdate datetime not null,
+	end_date datetime not null
 )
