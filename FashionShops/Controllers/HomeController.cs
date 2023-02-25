d5f63f21623f07f5f99e8a7b48b3146db5abe82c
Mr-Zero272 <pitithuong@gmail.com>
2023-02-25 12:13:43 +0700

    up file mvc

---
+++ FashionShops/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FashionShops.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
