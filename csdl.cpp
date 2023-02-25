cbacf6c8bb6856207b661360613843dc667b942d
Mr-Zero272 <pitithuong@gmail.com>
2023-02-25 22:58:22 +0700

    skirt finished

--- csdl.cpp
+++ csdl.cpp
@@ -30,28 +30,66 @@ void insertProductImage(int idPdStart, int quantity, int IdImageStart)
     }
 }
 
-void insertQuantity(int idPdStart, int quantity, int color1, int color2)
+void insertQuantity(int idPdStart, int quantity, int color1, int color2, int radomIndex)
 {
-    // 2 mau black 1 white 2 pink 3 blue 4 purple 5 tuy vay 1 sp lay 2 mau
+    // 2 mau black 1 white 2 pink 3 blue 4 purple 5 ,tuy vay 1 sp lay 2 mau
     // 4 size 1 2 3 4
     //(product_id, size_id, color_id, quantity)
-    int i = 0, j = 0, k = 1;
+    int i = 0, j = 1, k = 0;
     int c1 = color1;
     int c2 = color2;
-    int rad;
+    int rad1, rad2;
     for (i = idPdStart; i <= quantity; i++)
     {
         j = 1;
-        for(; j < 2; j++) // color
-        {
-            
+        for (; j < 5; j++) {
+            k = 0;
+            for (; k < 2; k++)
+            {
+                rad1 = 100 + (rand() % radomIndex);
+                //rad1 = 100 + (rand() % 1034);
+                if (k % 2 == 0)
+                    cout << "insert into Product_Quantity values ("<< i <<", "<< j <<", "<< color1 <<", "<< rad1 <<")" << endl;
+                else
+                    cout << "insert into Product_Quantity values ("<< i <<", "<< j <<", "<< color2 <<", "<< rad1 <<")" << endl;
+
+            }
         }
+    }
+}
+
+
+void insertProductTag(int idTag, int arrProductSameTag[], int quantity)
+{
+    int i = 0, j = 1;
+    for (i = 0; i < quantity; i++)
+    {
+        cout << "insert into Product_Tag values ("<< arrProductSameTag[i] <<", "<< idTag <<")" << endl;
+    }
+}
+
+void insertProductCategory(int arrProduct[], int quantity)
+{
+    int i = 0, j;
+    for (i = 0; i < quantity; i++)
+    {
+        j = 0;
+        for (j = 0; j < 2; j++) {
+            if (j % 2 == 0)
+                cout << "insert into Product_Category values("<< arrProduct[i] <<", "<< 4 <<")" << endl;
+            else
+                cout << "insert into Product_Category values("<< arrProduct[i] <<", "<< 5 <<")" << endl;
+        }
+        
     }
 }
 
 int main() {
     //insertImage(1, 1, 11);
     //insertProductImage(1, 10, 1);
-    insertQuantity(1, 1, 1, 2);
+    //insertQuantity(10, 10, 1, 3, 116);
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    //insertProductTag(8, a, 4);
+    insertProductCategory(a, 10);
     return 0;
 }
