49a3f486f8bff94d518c208eaa25544f9e5ed550
Mr-Zero272 <pitithuong@gmail.com>
2023-03-08 22:57:27 +0700

    tam tam vai chuc nang

---
+++ FashionShops/Controllers/Products/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FashionShops.Models;
+namespace FashionShops.Controllers.Products
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        FashionShopEntities data = new FashionShopEntities();
+        public ActionResult Index()
+        {
+
+            var query = from product in data.Products
+                        join proImage in data.Product_Image on product.product_id equals proImage.product_id
+                        join Img in data.Images on proImage.image_id equals Img.image_id
+                        where Img.image_id == (product.product_id - 1) * 3 + product.product_id
+                        select new ProductView
+                        {
+                            productName = product.name,
+                            productPrice = product.price,
+                            imageUrl = Img.imgae_url
+                        };
+            var model = query.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ FashionShops/Models/ProductView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FashionShops.Models
+{
+    public class ProductView
+    {
+        public string productName { get; set; }
+        public double productPrice { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
