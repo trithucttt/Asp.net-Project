c646fa92e40f416ce7c3cc7e3548becb40a1dc05
Mr-Zero272 <pitithuong@gmail.com>
2023-03-17 16:26:20 +0700

    fix them vai loi nho ben trang cart

---
+++ FashionShops/Controllers/Cart/CheckOutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FashionShops.Controllers.Cart
+{
+    public class CheckOutController : Controller
+    {
+        FashionShopEntities db = new FashionShopEntities();
+        // GET: CheckOut
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
