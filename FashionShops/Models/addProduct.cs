7f9a4bb3516c89da37b14d19b9bc8f066ed8f2d3
trithucttt <trithuc0416@gmail.com>
2023-03-23 19:52:54 +0700

    add product pages

---
+++ FashionShops/Models/addProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FashionShops.Models
+{
+    public class addProduct 
+    {
+        public long cart_id { get; set; }
+        public long user_id { get; set; }
+        public long product_id { get; set; }
+        public short quantity { get; set; }
+        public long size { get; set; }
+        public long color { get; set; }
+        public double total_price { get; set; }
+    }
+}
