3ca252e843dc5ed3488da20cbb079855de37264e
Mr-Zero272 <pitithuong@gmail.com>
2023-03-25 20:52:11 +0700

    pagination

--- FashionShops/Controllers/Cart/CheckOutController.cs
+++ FashionShops/Controllers/Cart/CheckOutController.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using FashionShops.Models.CheckOut;
-using FashionShops.Models.ListOrder;
 using FashionShops.Models.OrderTracking;
 using Newtonsoft.Json;
 
