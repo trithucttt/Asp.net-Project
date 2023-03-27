e9fbc47997a7189c313ae1f726bc22c7e9b1e143
longppk <hoanglong255l@gmail.com>
2023-03-27 16:54:07 +0700

    hien thi profile

---
+++ FashionShops/Controllers/Auth/ProfileController.cs
@@ -0,0 +1,44 @@
+﻿using FashionShops.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FashionShops.Controllers.Auth
+{
+    public class ProfileController : Controller
+    {
+        FashionShopEntities data = new FashionShopEntities();
+        public ActionResult Index()
+        {
+            if (Membership.GetUser() != null)
+            {
+                string userName = Membership.GetUser().UserName;
+                int userId = (int)data.Users.FirstOrDefault(x => x.username == userName).user_id;
+                return View(GetProfile(userId));
+            }
+            return View("~/Views/Login/showFormLogin.cshtml");
+        }
+
+        public Profile GetProfile(int userID)
+        {
+            var profile = (from p in data.Users
+                           where p.user_id == userID
+                           select new Profile
+                           {
+                               firstname = p.firstName,
+                               lastname = p.lastName,
+                               phonenumber = p.phoneNumber,
+                               email = p.email,
+                               username = p.username,
+                               address = p.address,
+                               province = p.province,
+                               city = p.city,
+                               country = p.country
+                           }).FirstOrDefault();
+            return profile;
+        }
+    }
+}
---
+++ FashionShops/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FashionShops.Models
+{
+    public class Profile
+    {
+        public string userId { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string phonenumber { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public string address { get; set; }
+        public string province { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+    }
+}
